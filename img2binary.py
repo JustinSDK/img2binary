fd71d6147c2d4d5c0103bdb35af9cf54f4e7f3bb
Justin Lin <justin.sdk@gmail.com>
2021-04-02 21:10:23 +0800

    refactor

--- img2binary.py
+++ img2binary.py
@@ -2,7 +2,7 @@
 # If you don’t have OpenCV installed, install it via pip:
 #     pip install opencv-python
 
-import sys, argparse
+import sys, argparse, numpy
 
 def main():
     parser = argparse.ArgumentParser()
@@ -17,16 +17,16 @@ def main():
             args = parser.parse_args()
             img = cv2.imread(args.src, 2)
             _, bw_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-            binary = [[1 if pixel == 255 else pixel for pixel in row] for row in bw_img]
-            leng = len(binary)
+
+            binary = numpy.where(bw_img == 255, 1, bw_img) 
             with open(args.dest, 'w') as dest:
                 dest.write('[\n')
-                for r in range(0, leng - 1):
+                for r in range(0, len(binary) - 1):
                     dest.write('\t')
-                    dest.write(str(binary[r]))
+                    dest.write(str(binary[r].tolist()))
                     dest.write(',\n')
                 dest.write('\t')
-                dest.write(str(binary[-1]))
+                dest.write(str(binary[-1].tolist()))
                 dest.write('\n]')
         except:
             print('If you don’t have OpenCV installed, install it via pip.')
